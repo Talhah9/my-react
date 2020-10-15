9d9f94d26333aadc4078c5763eaac50320366410
Talhah9 <talhahally974@gmail.com>
2020-10-15 16:13:41 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+// Dependencies
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.scss';
+const App = () => (
+  <div>
+    <h1>Hello</h1>
+  </div>
+);
+ReactDOM.render(<App />, document.getElementById('app'));
